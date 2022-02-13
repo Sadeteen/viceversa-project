dd1cc4ff02efeedafd65dc3de8c178b79c3b8640
Your Name <you@example.com>
2022-02-13 22:47:22 +0400

    Second commit

--- viceversa/urls.py
+++ viceversa/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('', views.home),
+    path('reversed/', views.reverse, name='reverse'),
 ]
--- viceversa/views.py
+++ viceversa/views.py
@@ -6,3 +6,9 @@ def about(request):
 
 def home(request):
 	return render(request, 'home.html', {'greeting':'Hello!'})
+
+def reverse(request):
+	user_text = request.GET['usertext']
+	reversed_user_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext':user_text, 'reversed_usertext':reversed_user_text})
+
